44d53a1f2abece27ffdec41860ddabc795cc1139
Saliou <farydioufsaliou@gmail.com>
2022-10-25 22:08:52 +0200

    H2 Database config (OK)

--- src/main/java/com/uca/series_temporelles/SeriesTemporellesApplication.java
+++ src/main/java/com/uca/series_temporelles/SeriesTemporellesApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SeriesTemporellesApplication {
+public class SeriesTemporellesApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SeriesTemporellesApplication.class, args);
