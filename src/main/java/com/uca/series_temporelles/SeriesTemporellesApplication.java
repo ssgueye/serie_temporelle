e6c98f5492b0697283b43ab7239acd4973fd0fac
Ismael_Diallo <dapieu99@gmail.com>
2022-10-25 13:02:45 +0200

    initial commit

---
+++ src/main/java/com/uca/series_temporelles/SeriesTemporellesApplication.java
@@ -0,0 +1,13 @@
+package com.uca.series_temporelles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeriesTemporellesApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeriesTemporellesApplication.class, args);
+	}
+
+}
